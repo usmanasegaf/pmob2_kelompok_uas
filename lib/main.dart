821af3052ad09d8c4a487d6e704f3944749ecc4c
MohammadUsmanAsegaf <usmanasegaf7@gmail.com>
2025-02-18 09:14:47 +0700

    splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pmob2_kelompok_uas/home.dart';
+import 'package:pmob2_kelompok_uas/splashscreen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Home(),
+      home: Splashscreen(),
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'dart:async';
+import 'package:pmob2_kelompok_uas/home.dart';
 
-class Spashscreen extends StatefulWidget {
-  const Spashscreen({super.key});
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
 
   @override
-  State<Spashscreen> createState() => _SpashscreenState();
+  State<Splashscreen> createState() => _SpashscreenState();
 }
 
-class _SpashscreenState extends State<Spashscreen> {
+class _SpashscreenState extends State<Splashscreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(milliseconds: 2700), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const Home()),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Lottie.asset(
+          'lib/assets/QrAnimation.json',
+          width: 360,
+          height: 360,
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
   }
 }
